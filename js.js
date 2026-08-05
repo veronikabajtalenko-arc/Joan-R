36876db6075f3f53f57504562fa3d4599d2e0d81
Veronika <veronikabajtalenko@gmail.com>
2026-08-05 19:07:29 +0300

    с

--- js.js
+++ js.js
@@ -1,3 +1,36 @@
+
+let textSize = 16;
+let isHightContrast = false;
+const increaseTextButton = document.getElementById(`increaseTextButton`);
+const increaseText = document.getElementById('toggleContrast');
+
+const toggleContrastButton = document.getElementById('toggleContrastButton');
+toggleContrastButton.onclick = function () {
+    isHightContrast = !isHightContrast;
+    if (isHightContrast) {
+        document.body.style.backgroundColor = `#000`;
+        document.body.style.color = `#f8f6f0`;
+    } else {
+        document.body.style.backgroundColor = ``;
+        document.body.style.color = ``;
+    }
+}
+
+
+increaseTextButton.onclick = function () {
+    textSize += 2;
+    document.body.style.fontSize = textSize + 'px';
+}
+console.log(increaseTextButton)
+
+
+
+
+
+
+
+
+
 document.querySelector('order-form').addEventListener('submit', function (e) {
     e.preventDefault();
     console.log('Ваш Email:', document.getElementById('user-text').value);
