627e78e077c71b01b16b1e97d3999a47e3e5945b
Veronika <veronikabajtalenko@gmail.com>
2026-08-07 14:59:15 +0300

    c

--- js.js
+++ js.js
@@ -33,9 +33,9 @@ console.log(increaseTextButton)
 
 document.querySelector('order-form').addEventListener('submit', function (e) {
     e.preventDefault();
-    console.log('Ваш Email:', document.getElementById('user-text').value);
-    console.log('Номер телефону:', document.getElementById('user-text').value);
-    alert('Інформацію успішно ндіслано! Чекайте на повідомлення чи дзвінок.');
+    console.log('Ваш Email:', document.getElementById('email').value);
+    console.log('Номер телефону:', document.getElementById('email').value);
+    alert('Чудово!Чекайте на повідомлення або дзвінок.');
 });
 function sharePage() {
     navigator.share(
